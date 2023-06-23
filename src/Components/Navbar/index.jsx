9d23aee618e0e2bd1a676e6120270c361100889e
edpadua <ed.padua@gmail.com>
2023-06-23 12:56:19 -0300

    First commit
    First commit

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className="bg-sky-600 top-0 left-0 right-0 w-full h-24 px-20 pt-8">
+
+    </nav>
+  )
+}
+
+export default Navbar
